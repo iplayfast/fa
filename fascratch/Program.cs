628449a9bf144d17539cfe46dacc21f3d92648f5
codewitch honey crisis <gaztoast@gmail.com>
2019-07-30 16:46:29 -0700

    Add project files.

---
+++ fascratch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FA
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var fa = CharFA<string>.Parse("hello|world!", "success!");
+			fa.RenderToFile(@"..\..\..\helloworld.jpg");
+			Console.WriteLine(fa);
+
+			var tok = fa.Lex("helloworld".GetEnumerator(), "#ERROR");
+			Console.WriteLine("{0}: {1}", tok.Key, new string(tok.Value));
+		}
+	}
+}
